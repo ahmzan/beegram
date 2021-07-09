6b1094d5c210b082df052894141f75bab642aa7e
Ahmzan <ahmzan.dev@gmail.com>
2021-07-09 20:17:16 +0700

    Create TLObject

---
+++ src/TL/Core/TLObject.ts
@@ -0,0 +1,22 @@
+import { Int } from './Primitive';
+import { tlObject } from '../All';
+
+export class TLObject {
+  constructor() {}
+  write() {}
+
+  static read(data: Buffer, type?: any) {
+    // slice constructor
+    const body = data.slice(4);
+
+    const constructId = Int.read(data, 0, true) >>> 0
+    const tlClass = tlObject[constructId];
+
+    try {
+      if (type) return tlClass.read(body, type);
+      return tlClass.read(body);
+    } catch (err) {
+      return 'type uknown';
+    }
+  }
+}
