ed5d3275c4675e190783160893ff9cedc3eeae5e
Ahmzan <ahmzan.dev@gmail.com>
2021-07-09 20:18:21 +0700

    Export primitve

---
+++ src/TL/Core/index.ts
@@ -0,0 +1,15 @@
+export {
+  int,
+  Int,
+  Int32,
+  Long,
+  long,
+  Int64,
+  Int128,
+  Int256,
+  Double,
+  Bytes,
+  Str,
+  Vector,
+} from './Primitive';
+export { TLObject } from './TLObject';
