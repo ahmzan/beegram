5511a4d16fd6bd573bb6ddc09a97906766f1015f
Ahmzan <ahmzan.dev@gmail.com>
2021-09-06 21:10:46 +0700

    Update connection

--- src/Connection/index.ts
+++ src/Connection/index.ts
@@ -1,43 +1,47 @@
 import { Datacenter } from './Datacenter';
 import { TransportError } from '../Errors/Transport';
 import { TCPAmbridge, TCPIntermediate } from './Transport/TCP/';
+import { promisify } from 'util';
 import debug from 'debug';
 
 const log = debug('Connection');
+const wait = promisify(setTimeout);
 
 export class Connection {
   dcId: number;
   testMode: boolean;
   address: string;
   protocol: 'TCP' | 'WS' | 'HTTP';
-  transport: TCPAmbridge | TCPIntermediate;
+  transport!: TCPAmbridge | TCPIntermediate;
   retries: number;
   mode: 'TCPAmbridge' | 'TCPIntermediate' | 'TCPFull';
   constructor(dcId: number, testMode: boolean) {
+    log('Init connection %o %o', dcId, testMode);
     this.dcId = dcId;
     this.testMode = testMode;
 
     this.address = Datacenter(this.dcId, testMode);
     this.protocol = 'TCP';
     this.mode = 'TCPAmbridge';
-    this.transport = new TCPIntermediate(this.address);
 
     this.retries = 5;
-    log('Creating connection');
   }
 
   async connect() {
-    for (var i = 0; i < this.retries; i++) {
+    for (var i = 1; i <= this.retries; i++) {
       this.transport = new TCPIntermediate(this.address);
 
+      if (i == this.retries)
+        throw Error('Fail connecting to server please check your internet connection');
       try {
         log('Connecting to %s', this.address);
         await this.transport.connect();
         break;
       } catch (err) {
         log('Failed to connect %o', err);
-        this.transport.close();
-        continue;
+        await this.transport.close();
+        console.log(err.message, 'Retriying');
+        await wait(2000);
       }
     }
   }
@@ -52,11 +56,19 @@ export class Connection {
     await this.transport.send(payload);
   }
 
+  onData(fn: (data: Buffer) => Promise<void>) {
+    this.transport.onData(fn);
+  }
+
   async receive() {
     const payload = await this.transport.receive();
     log('Receving payload');
     if (payload.length == 4) {
-      throw new TransportError(payload.readInt32LE().toString());
+      const errCode = payload.readInt32LE().toString();
+      if (errCode == '-404') {
+        throw new TransportError('404 Incorrect query');
+      }
+      throw new TransportError(errCode);
     }
     return payload;
   }
