6eecb20bba0becfad51a24b29a39e8dcf7f64735
Ahmzan <ahmzan.dev@gmail.com>
2021-09-06 21:22:30 +0700

    Update FileStorage

--- src/Storage/FileStorage.ts
+++ src/Storage/FileStorage.ts
@@ -1,90 +1,161 @@
+import { getInputPeer, PeerData, SessionStorage } from './SessionStorage';
+import { InputPeer } from '../TL/Schema';
+import debug from 'debug';
 import * as fs from 'fs';
-import { SessionStorage } from './';
 
+const log = debug('FileStorage');
+
+/**
+ * a class that stores session data in file
+ **/
 export class FileStorage extends SessionStorage {
-  database: string;
-  storage: any;
+  path!: string;
+  db: any;
   constructor(name: string) {
     super(name);
 
-    this.database = `${this.name}.session`;
-    this.storage = {};
+    this.path = name.endsWith('/') ? name : name + '/';
+    this.open();
   }
 
+  /**
+   * Checking folder data
+   **/
   open() {
-    const path = this.database;
-
-    if (fs.existsSync(path)) {
-      this.storage = JSON.parse(fs.readFileSync(path, 'utf-8'));
-    } else {
-      this.create();
-    }
+    if (!fs.existsSync(this.path)) this.create();
   }
 
-  close() {
-    this.save();
-    this.storage = {};
-  }
+  close() {}
 
-  save() {
-    fs.writeFileSync(this.database, JSON.stringify(this.storage));
-  }
+  save() {}
 
+  /**
+   * Create a folder data
+   **/
   create() {
-    fs.writeFileSync(this.database, JSON.stringify({}));
+    fs.mkdirSync(this.name);
   }
 
+  /**
+   * Destroy folder data
+   **/
+  delete() {
+    fs.unlinkSync(this.name);
+  }
+
+  /**
+   * Get and Save authkey
+   * @return return a Buffer
+   **/
+  authKey(key?: Buffer): Buffer {
+    if (key != undefined) {
+      fs.writeFileSync(this.path + 'authkey', key.toString());
+    }
+
+    return Buffer.from(fs.readFileSync(this.path + 'authkey', 'utf-8'));
+  }
+
+  /**
+   * Get and Save date
+   * @return return a number
+   **/
+  date(date?: number): number {
+    if (date != undefined) {
+      fs.writeFileSync(this.path + 'date', date.toString());
+    }
+
+    return parseInt(fs.readFileSync(this.path + 'date', 'utf-8'));
+  }
+
+  /**
+   * Get and Save dcid
+   * @return return a number
+   **/
+  dcId(dcId?: number): number {
+    if (dcId != undefined) {
+      fs.writeFileSync(this.path + 'dcid', dcId.toString());
+    }
+
+    return parseInt(fs.readFileSync(this.path + 'dcid', 'utf-8'));
+  }
+
+  /**
+   * Get and Save isbot
+   * @return return a boolean
+   **/
+  isBot(isBot?: boolean): boolean {
+    if (isBot != undefined) {
+      fs.writeFileSync(this.path + 'isbot', isBot.toString());
+    }
+
+    return fs.readFileSync(this.path + 'isbot', 'utf-8') == 'true' ? true : false;
+  }
+
+  /**
+   * Get and Save testmode
+   * @return return a boolean
+   **/
   testMode(test?: boolean) {
-    if (test == undefined) {
-      return this.storage['test-mode'];
+    if (test != undefined) {
+      fs.writeFileSync(this.path + 'testmode', test.toString());
     }
-    this.storage['test-mode'] = test;
-    this.save();
-    return this.storage['test-mode'];
+
+    return fs.readFileSync(this.path + 'testmode', 'utf-8') == 'true' ? true : false;
   }
 
-  authKey(key?: string) {
-    if (key == undefined) {
-      return this.storage['auth-key'];
+  /**
+   * Get and Save userid
+   * @return return a number
+   **/
+  userId(userId?: number): number {
+    if (userId != undefined) {
+      fs.writeFileSync(this.path + 'dcid', userId.toString());
     }
-    this.storage['auth-key'] = key;
-    this.save();
-    return this.storage['auth-key'];
+
+    return parseInt(fs.readFileSync(this.path + 'dcid', 'utf-8'));
   }
 
-  userId(id?: number) {
-    if (id == undefined) {
-      return this.storage['user-id'];
-    }
-    this.storage['user-id'] = id;
-    this.save();
-    return this.storage['user-id'];
+  /**
+   * Get peer data by id
+   **/
+  getPeerById(id: number): InputPeer {
+    const peerData = this.db.prepare(`SELECT * FROM peers WHERE peer_id = ${id}`).get();
+
+    return getInputPeer(peerData);
   }
 
-  isBot(is?: boolean) {
-    if (is == undefined) {
-      return this.storage['is-bot'];
-    }
-    this.storage['is-bot'] = is;
-    this.save();
-    return this.storage['is-bot'];
+  /**
+   * Get peer data by phone number with international format (e.g 1234, 628111)
+   **/
+  getPeerByPhoneNumber(phone: number): InputPeer {
+    const peerData = this.db.prepare(`SELECT * FROM peers WHERE phone_number = ${phone}`).get();
+
+    return getInputPeer(peerData);
   }
 
-  date(date?: number) {
-    if (date == undefined) {
-      return this.storage['date'];
-    }
-    this.storage['dat'] = date;
-    this.save();
-    return this.storage['date'];
+  /**
+   * Get peer data by username without @ symbol
+   **/
+  getPeerByUsername(username: string): InputPeer {
+    const peerData = this.db.prepare('SELECT * FROM peers WHERE username = ?').get(username);
+
+    return getInputPeer(peerData);
   }
 
-  dcId(dcId?: number) {
-    if (dcId == undefined) {
-      return this.storage['dc-id'];
-    }
-    this.storage['dc-id'] = dcId;
-    this.save();
-    return this.storage['dc-id'];
+  /**
+   * Update peer with given data
+   **/
+  updatePeer(peerData: PeerData) {
+    this.db
+      .prepare(
+        `REPLACE INTO peers (peer_id, access_hash, type, username, phone_number) VALUES (?, ?, ?, ?, ?)`
+      )
+      .run(
+        peerData.peer_id,
+        peerData.access_hash,
+        peerData.type,
+        peerData.username,
+        peerData.phone_number
+      );
   }
 }
