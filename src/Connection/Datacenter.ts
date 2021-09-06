54a389bb168ab43a7701509d0f6af60197534051
Ahmzan <ahmzan.dev@gmail.com>
2021-09-06 21:04:20 +0700

    Throw error if dc id outside list

--- src/Connection/Datacenter.ts
+++ src/Connection/Datacenter.ts
@@ -7,12 +7,18 @@ export function Datacenter(dcId: number, test?: boolean): string {
 
   const PROD: { [key: number]: string } = {
     1: '149.154.175.53',
-    2: '149.154.167.51',
+    2: '149.154.167.50',
     3: '149.154.175.100',
     4: '149.154.167.91',
     5: '91.108.56.130'
   };
 
-  if (test) return TEST[dcId];
+  if (test) {
+    if (TEST[dcId] == undefined)
+      throw Error(`Datacenter ${dcId} is not available for test mode`);
+    return TEST[dcId];
+  }
+  if (PROD[dcId] == undefined)
+    throw Error(`Datacenter ${dcId} is not available for production mode`);
   return PROD[dcId];
 }
