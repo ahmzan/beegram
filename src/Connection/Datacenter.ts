2d09460fd420b974a288d5cf250a71891fb6a0e4
Ahmzan <ahmzan.dev@gmail.com>
2021-09-08 15:50:24 +0700

    Put LICENSE notice

--- src/Connection/Datacenter.ts
+++ src/Connection/Datacenter.ts
@@ -1,3 +1,23 @@
+/* 
+beegram - Telegram MTProto API Client Framework
+Copyright (C) 2021 Ahmzan <https://github.com/ahmzan>
+
+This file is part of beegram.
+
+beegram is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+beegram is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with beegram.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 export function Datacenter(dcId: number, test?: boolean): string {
   const TEST: { [key: number]: string } = {
     1: '149.154.175.10',
@@ -14,8 +34,7 @@ export function Datacenter(dcId: number, test?: boolean): string {
   };
 
   if (test) {
-    if (TEST[dcId] == undefined)
-      throw Error(`Datacenter ${dcId} is not available for test mode`);
+    if (TEST[dcId] == undefined) throw Error(`Datacenter ${dcId} is not available for test mode`);
     return TEST[dcId];
   }
   if (PROD[dcId] == undefined)
--- src/Session/Auth.ts
+++ src/Session/Auth.ts
@@ -1,3 +1,23 @@
+/* 
+beegram - Telegram MTProto API Client Framework
+Copyright (C) 2021 Ahmzan <https://github.com/ahmzan>
+
+This file is part of beegram.
+
+beegram is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+beegram is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with beegram.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { Connection } from '../Connection';
 import { Int, Long } from '../TL/Core/Primitive';
 import { TLObject } from '../TL/Core/TLObject';
