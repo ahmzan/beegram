05bd0784438f78a8226ec67f85d092c31ae87d21
Ahmzan <ahmzan.dev@gmail.com>
2021-10-12 16:23:10 +0700

    Create Peer, RPC, Timeout error

---
+++ src/Errors/RPCError.ts
@@ -0,0 +1,32 @@
+/* 
+beegram - Telegram MTProto API Client Framework
+Copyright (C) 2021 Ahmzan <https://github.com/ahmzan>
+
+This file is part of beegram.
+
+beegram is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+beegram is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with beegram.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { rpc_error } from '../TL/Proto';
+
+export class RPCError extends Error {
+  code: number;
+  constructor(rpcError: rpc_error) {
+    super();
+
+    this.name = 'RPCError';
+    this.code = rpcError.error_code;
+    this.message = rpcError.error_message;
+  }
+}
